3068ad74d33aa2bee5e6da15af47a8de2fa0c5da
anamika suresh <anamikasuresh791@gmail.com>
2026-07-01 15:42:19 +0530

    fix heritage images

--- store/models.py
+++ store/models.py
@@ -28,6 +28,7 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    size = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return f"{self.user} - {self.product}"
@@ -51,6 +52,7 @@ class Order(models.Model):
     address = models.TextField(default='')
     city = models.CharField(max_length=100, default='')
     pincode = models.CharField(max_length=10, default='')
+    size = models.CharField(max_length=10)
 
     def __str__(self):
         return f"{self.user} - {self.product}"
